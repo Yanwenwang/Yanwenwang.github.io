435359e3ee4650ab3c84153b370501f319518e11
Yanwen <ywwenwang@gmail.com>
2015-05-19 23:31:57 -0700

    added jshint

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
  
 var paths = {
+  js: './src/**/*.js',
   scss: './src/**/*.scss'
 };
 
@@ -15,10 +18,18 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./src/'));
 });
  
+ gulp.task('lint', function() {
+  return gulp.src(paths.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
 // Rerun the task when a file changes 
 gulp.task('watch', function() {
   gulp.watch(paths.scss, ['sass']);
+  gulp.watch(paths.js, ['lint']);
 });
  
 // The default task (called when you run `gulp` from cli) 
 gulp.task('default', ['watch']);
+ 
