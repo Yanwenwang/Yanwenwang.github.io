52cb7148fda1d28829db6c558c3200c907efc07d
Yanwen <ywwenwang@gmail.com>
2015-05-21 22:45:32 -0700

    changed location of index.html

--- src/app/routes.js
+++ src/app/routes.js
@@ -10,27 +10,27 @@
 
 		$routeProvider
 			.when('/', {
-				templateUrl: './app/home/html/home.html',
+				templateUrl: './src/app/home/html/home.html',
                 controller: 'homeController',
                 controllerAs: 'homeVM'
 			})
 			.when('/resume', {
-				templateUrl: './app/resume/html/resume.html',
+				templateUrl: './src/app/resume/html/resume.html',
 				controller: 'resumeController',
 				controllerAs: 'resumeVM'
 			})
 			.when('/projects', {
-				templateUrl: './app/projects/html/projects.html',
+				templateUrl: './src/app/projects/html/projects.html',
 				controller: 'projectsController',
 				controllerAs: 'projectsVM'
 			})
 			.when('/about', {
-				templateUrl: './app/about/html/about.html',
+				templateUrl: './src/app/about/html/about.html',
 				controller: 'aboutController',
 				controllerAs: 'aboutVM'
 			})
 			.when('/contact', {
-				templateUrl: './app/contact/html/contact.html',
+				templateUrl: './src/app/contact/html/contact.html',
 				controller: 'contactController',
 				controllerAs: 'contactVM'
 			})
