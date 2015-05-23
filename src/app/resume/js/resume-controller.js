ac81e3e9cd74c895febdc20ca51bdfbdd692af5d
Yanwen <ywwenwang@gmail.com>
2015-05-22 20:18:56 -0700

    added skills to resume

--- src/app/resume/js/resume-controller.js
+++ src/app/resume/js/resume-controller.js
@@ -1,14 +1,82 @@
-(function () {
+(function() {
 
 	angular
 		.module('ywPortal')
 		.controller('resumeController', resumeController);
 
-	function resumeController () {
-
+	function resumeController() {
 		var vm = this;
-		vm.greeting = 'Hello resume!';
-	
+
+		vm.groups = [
+			{
+				groupName: 'HTML'
+			},
+			{ 
+				groupName: 'CSS', 
+				skills: [ 
+					{ name: 'Sass' },
+					{ name: 'Box model' },
+					{ name: 'Box model' },
+		 			{ name: 'Bootstrap' },
+		 			{ name: 'Animate.css' }
+				]
+			},
+			{
+				groupName: 'JavaScript',
+				skills: [
+					{ name: 'IIFE' },
+					{ name: 'Closure' },
+					{ name: 'Hoisting' },
+					{ name: 'Scope' },
+				 	{ name: 'JSON' },
+					{ name: 'Callbacks' },
+					{ name: 'Truthy/Falsy'}
+				]
+			},
+			{
+				groupName: 'AngularJS',
+				skills: [
+					{ name: 'Directives' },
+					{ name: 'controllers' },
+				 	{ name: 'controllerAs' },
+				 	{ name: 'Services' },
+				 	{ name: 'Routing' },
+				 	{ name: 'ng-class' },
+				 	{ name: 'ng-bind' },
+				 	{ name: '$http' },
+				 	{ name: '$location' }
+				]
+			},
+			{
+				groupName: 'Git',
+				skills: [
+					{ name: 'Github' },
+				    { name: 'SourceTree' }
+				]
+			},
+			{
+				groupName: 'Mac',
+				skills: [
+					{ name: 'Terminal' },
+				 	{ name: 'npm' },
+				 	{ name: 'Bower' },
+				 	{ name: 'Jshint' }
+				]
+			},
+			{
+				groupName: 'IDEs',
+				skills: [
+					{ name: 'Sublime Text' }
+				]
+			},
+			{
+				groupName: 'Methodologies',
+				skills: [
+					{ name: 'Scrum' }
+				]
+			}
+		];
+
 	}
 
 }());
