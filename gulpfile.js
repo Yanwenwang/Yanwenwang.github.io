621fa7cde85cf41252a7406f93c9ac0daeec04cf
Yanwen <ywwenwang@gmail.com>
2015-06-13 18:18:42 -0700

    added browsersync dependency, and integrated with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,45 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var browserSync = require('browser-sync').create();
  
 var paths = {
   js: './src/**/*.js',
-  scss: './src/**/*.scss'
+  scss: './src/**/*.scss',
+  html: './src/**/*.html'
 };
 
 gulp.task('sass', function () {
   gulp.src(paths.scss)
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      indentedSyntax: 'true',
+      sourceComments:'true'
+    }).on('error', sass.logError))
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('./src/'));
+    .pipe(gulp.dest('./src/'))
+    .pipe(browserSync.stream());
+
 });
  
- gulp.task('lint', function() {
+gulp.task('lint', function() {
   return gulp.src(paths.js)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
-// Rerun the task when a file changes 
-gulp.task('watch', function() {
+// Static server
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
   gulp.watch(paths.scss, ['sass']);
   gulp.watch(paths.js, ['lint']);
+  gulp.watch(paths.html).on('change', browserSync.reload);
 });
- 
+
 // The default task (called when you run `gulp` from cli) 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve']);
  
