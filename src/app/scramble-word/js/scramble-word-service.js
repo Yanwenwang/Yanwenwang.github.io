b0f53f01f58b186c655c2ae594bbf371720d8fe7
Yanwen <ywwenwang@gmail.com>
2015-07-09 23:31:27 -0700

    added code to make game work in scramble word service

--- src/app/scramble-word/js/scramble-word-service.js
+++ src/app/scramble-word/js/scramble-word-service.js
@@ -4,31 +4,114 @@
 		.module('ywPortal')
 		.factory('scrambleWordService', scrambleWordService);
 
-        scrambleWordService.$inject = ['$http'];
+        scrambleWordService.$inject = ['scrambleWordDataService', 'commonService'];
 
-        function scrambleWordService ($http) {
-
-            var wordnikUrl = "http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=1000000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=8&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        function scrambleWordService (scrambleWordDataService, commonService) {
 
             var service = {
-                getNewWord: getNewWord
+				checkKeyPress: checkKeyPress
             };
 
             return service;
 
-            function getNewWord () {
-                return $http.get(wordnikUrl)
-                    .then(function (response) {
-                        if(!response || response.data || response.data.word) {
-                            // open modal for unable to get word from wordnik
-                        }
+			function checkKeyPress(which) {
 
-                        return response.data.word;
-                    })
-                    .catch(function (error) {
-                        // open modal for unable to get word from wordnik
-                    });
-            }
+				// check to see if alphabetic letter was pressed
+				var letterPressed = commonService.getLetterPressed(which);
+
+				// if no letter was pressed, return
+				if(!letterPressed) {
+					return;
+				}
+
+				// if letter was pressed, check to see if it is valid
+				checkValidLetter(letterPressed);
+			}
+
+			function checkValidLetter(letterPressed) {
+
+				var validLetterArr = [];
+
+				// find all letters that have not been typed and push into valid letter array
+				// for loop starts at typed letters index
+				for(var i = scrambleWordDataService.data.letterTypedCount; i < scrambleWordDataService.data.scrambledWordArray.length; i ++) {
+					validLetterArr.push(scrambleWordDataService.data.scrambledWordArray[i].letter);
+				}
+
+				// check to see if valid letter in the not typed yet array
+				var indexOfLetterPressInValidLetterArray = validLetterArr.indexOf(letterPressed);
+
+				// if no valid letter pressed, return
+				if(indexOfLetterPressInValidLetterArray === -1) {
+					return;
+				}
+
+				// add type count back to index, since index is actually of just non-typed letters, not whole array
+				var indexOfLetterPressedInScrambledArray = indexOfLetterPressInValidLetterArray + scrambleWordDataService.data.letterTypedCount;
+
+				// move letter to correct position
+				moveLetter(indexOfLetterPressedInScrambledArray);
+			}
+
+			function moveLetter(indexOfLetterPressedInScrambledArray) {
+
+				// splice pressed letter object out of array
+				var splicedElementArray = scrambleWordDataService.data.scrambledWordArray.splice(indexOfLetterPressedInScrambledArray, 1);
+
+				// splice returns array, take first element
+				var splicedElement = splicedElementArray[0];
+
+				// style letter through ng-class by making typed true
+				splicedElement.typed = true;
+
+				// splice letter object into array, so that all elements are pushed down
+				scrambleWordDataService.data.scrambledWordArray.splice(scrambleWordDataService.data.letterTypedCount, 0, splicedElement);
+
+				// increase letter typed count
+				scrambleWordDataService.data.letterTypedCount++;
+
+				// check for win condition
+				checkForWinCondition();
+			}
+
+			function checkForWinCondition() {
+
+				if(scrambleWordDataService.data.letterTypedCount !== scrambleWordDataService.data.scrambledWordArray.length) {
+					// if letters typed is not equal to word length, return
+					return;
+				}
+
+				var match = checkIfWordMatches();
+
+				if(match) {
+					// show modal for correct word
+
+					// reset the data so everything is back to empty
+					scrambleWordDataService.resetGame();
+
+					// get new word, which will start the game over
+					scrambleWordDataService.getNewWord();
+				} else {
+					// show modal for wrong word
+
+					// reset round
+					scrambleWordDataService.resetRound();
+				}
+			}
+
+			function checkIfWordMatches() {
+				// loop through array to check to see if any of the
+				for(var i = 0; i < scrambleWordDataService.data.scrambledWordArray.length; i ++) {
+
+					// if the letters do not match is the scrambledArray and the original array, return false
+					if(scrambleWordDataService.data.scrambledWordArray[i].letter != scrambleWordDataService.data.wordArray[i]) {
+						return false;
+					}
+				}
+
+				// if all letters match, return true
+				return true;
+			}
         }
 
 }());
