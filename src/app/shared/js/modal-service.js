1b3fb656fa7c34a0adf2f32671ca322d35ecf9c4
Yanwen <ywwenwang@gmail.com>
2015-07-09 23:40:47 -0700

    added modal service, need to replace tic tac toe modals later

---
+++ src/app/shared/js/modal-service.js
@@ -0,0 +1,32 @@
+(function () {
+
+    angular
+        .module('ywPortal')
+        .factory('modalService', modalService);
+
+    modalService.$inject = ['$modal']
+
+    function modalService($modal) {
+
+        var service = {
+            openMessageModal: openMessageModal
+        };
+
+        return service;
+
+        function openMessageModal(data) {
+            $modal.open({
+                animation: true,
+                templateUrl: './src/app/shared/html/message-modal.html',
+                controller: 'modalInfoController',
+                controllerAs: 'modalInfoVM',
+                size: 'sm',
+                resolve: {
+                    data: function () {
+                        return data;
+                    }
+                }
+            });
+        }
+    }
+}());
