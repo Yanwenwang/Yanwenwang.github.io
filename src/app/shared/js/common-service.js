719987276c67d74ab7a688ff3cbd0afc2b41de72
Yanwen <ywwenwang@gmail.com>
2015-07-09 23:07:18 -0700

    added comments to code to make reasoning more clear

--- src/app/shared/js/common-service.js
+++ src/app/shared/js/common-service.js
@@ -14,18 +14,24 @@
 
         return service;
 
+        // this returns a random number between min and max, not including max
         function getRandomInt(min, max) {
             return Math.floor(Math.random() * (max - min)) + min;
         }
 
+        // this scrambles an array by randomly splicing an element out of the array and then pushing it into another array
         function scrambleArray(arr) {
+
+            // need to create copy since arr is a reference type
+            // modifying arr will modify the actual arr
+            var copyArr = arr.slice();
 
             var scrambledArray = [];
 
-            while (arr.length > 0) {
+            while (copyArr.length > 0) {
 
-                var randomIndex = getRandomInt(0, arr.length);
-                var randomElement = arr.splice(randomIndex, 1)[0];
+                var randomIndex = getRandomInt(0, copyArr.length);
+                var randomElement = copyArr.splice(randomIndex, 1)[0];
 
                 scrambledArray.push(randomElement);
             }
@@ -33,20 +39,21 @@
             return scrambledArray;
         }
 
+        // this functon will turn a event.which number from keydown into a letter
         function getLetterPressed(which) {
 
             // which codes for a-z are 65-90
-
             if(which < 65 || which > 90) {
                 return;
             }
 
-            var arr = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+            var letterArray = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
 
+            // since 65 is a, we just need to subtract 65 and return index of letterArray
             var index = which - 65;
 
-            return arr[index];
+            return letterArray[index];
         }
-    }
 
+    }
 }());
