28d4eac31fcccf4f069bce0d7aa6877fef2c40e1
Yanwen <ywwenwang@gmail.com>
2018-03-06 22:39:07 -0800

    Remove footer from HomeView and move it to CoreLayout.

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import BackgroundCalloutContainer from './BackgroundCalloutContainer';
 import ProgrammingCalloutContainer from './ProgrammingCalloutContainer';
-import Footer from '../Footer/Footer';
 
 export const HomeView = () => {
     return (
         <React.Fragment>
             <BackgroundCalloutContainer />
             <ProgrammingCalloutContainer />
-            <Footer />
         </React.Fragment>
     );
 };
--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -3,12 +3,14 @@ import React from 'react';
 import '../../styles/core.scss';
 
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 
 export const CoreLayout = ({ children }) => {
     return (
         <React.Fragment>
             <Navbar />
             {children}
+            <Footer />
         </React.Fragment>
     );
 }
