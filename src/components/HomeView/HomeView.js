b7bc561af868a31234bf6d54b3d71a4b2f18517d
Yanwen <ywwenwang@gmail.com>
2018-03-12 10:30:41 -0700

    Update home view.

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import BackgroundCalloutContainer from './BackgroundCalloutContainer';
-import ProgrammingCalloutContainer from './ProgrammingCalloutContainer';
-import TodayCalloutContainer from './TodayCalloutContainer';
+// import ProgrammingCalloutContainer from './ProgrammingCalloutContainer';
+// import TodayCalloutContainer from './TodayCalloutContainer';
 
 export const HomeView = () => {
     return (
         <React.Fragment>
             <BackgroundCalloutContainer />
-            <ProgrammingCalloutContainer />
-            <TodayCalloutContainer />
         </React.Fragment>
     );
 };
