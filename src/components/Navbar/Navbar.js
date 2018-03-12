9ea4daaf83cb6620e2b2e755ed4deea28f021d44
Yanwen <ywwenwang@gmail.com>
2018-03-12 10:29:06 -0700

    Update navbar styling.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,19 +6,21 @@ import './Navbar.scss';
 
 export const Navbar = () => {
     return (
-        <Container>
-        	<nav className="navbar__container">
-                <Link className="navbar__logo" to="/">.YW</Link>
-                <ul className="navbar__group">
-                    <li>
-                    	<Link className="navbar__item" to="/">Home</Link>
-                    </li>
-                    <li>
-                    	<Link className="navbar__item" to="/resume">Resume</Link>
-                    </li>
-                </ul>
-             </nav>
-         </Container>
+        <div className="navbar__outer-container">
+            <Container>
+            	<nav className="navbar__container">
+                    <Link className="navbar__logo" to="/">.YW</Link>
+                    <ul className="navbar__group">
+                        <li>
+                        	<Link className="navbar__item" to="/">Home</Link>
+                        </li>
+                        <li>
+                        	<Link className="navbar__item" to="/resume">Resume</Link>
+                        </li>
+                    </ul>
+                 </nav>
+             </Container>
+         </div>
     )
 };
 
