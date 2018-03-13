45aa72d10a9b160db40d2102fe6ddb557308b281
Yanwen <ywwenwang@gmail.com>
2018-03-13 11:48:32 -0700

    Make icons smaller in footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,13 +8,13 @@ const Footer = () => {
             <p className="footer__license">Copyright © 2018 Yanwen Wang</p>
             <div>
                 <a href="https://www.linkedin.com/in/yanwenwang" rel="noopener noreferrer" target="_blank" className="footer__icon">
-                    <i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+                    <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
                 </a>
                 <a href="https://github.com/Yanwenwang" rel="noopener noreferrer" target="_blank" className="footer__icon">
-                    <i className="fa fa-github fa-3x" aria-hidden="true"></i>
+                    <i className="fa fa-github fa-2x" aria-hidden="true"></i>
                 </a>
                 <a href="https://www.instagram.com/ywphotos" rel="noopener noreferrer" target="_blank" className="footer__icon footer__icon--last">
-                    <i className="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                    <i className="fa fa-instagram fa-2x" aria-hidden="true"></i>
                 </a>
             </div>
         </footer>
