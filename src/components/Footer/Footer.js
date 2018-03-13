6d8debea98f2bde5163a05ce0f30ff0add9d743b
Yanwen <ywwenwang@gmail.com>
2018-03-13 12:05:51 -0700

    Update icons styling.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,14 +7,14 @@ const Footer = () => {
         <footer className="footer__container">
             <p className="footer__license">Copyright © 2018 Yanwen Wang</p>
             <div>
-                <a href="https://www.linkedin.com/in/yanwenwang" rel="noopener noreferrer" target="_blank" className="footer__icon">
-                    <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+                <a href="https://www.linkedin.com/in/yanwenwang" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-linkedin-square fa-2x footer__icon footer__icon--linkedin" aria-hidden="true"></i>
                 </a>
-                <a href="https://github.com/Yanwenwang" rel="noopener noreferrer" target="_blank" className="footer__icon">
-                    <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+                <a href="https://github.com/Yanwenwang" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-github fa-2x footer__icon footer__icon--github" aria-hidden="true"></i>
                 </a>
-                <a href="https://www.instagram.com/ywphotos" rel="noopener noreferrer" target="_blank" className="footer__icon footer__icon--last">
-                    <i className="fa fa-instagram fa-2x" aria-hidden="true"></i>
+                <a href="https://www.instagram.com/ywphotos" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-instagram fa-2x footer__icon footer__icon--instagram" aria-hidden="true"></i>
                 </a>
             </div>
         </footer>
