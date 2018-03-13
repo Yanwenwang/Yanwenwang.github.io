a0c95a26c5b42582785a97bc8b3f7cb0b2d640d0
Yanwen <ywwenwang@gmail.com>
2018-03-13 00:31:32 -0700

    Add social media icons and links.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,17 @@ const Footer = () => {
     return (
         <footer className="footer__container">
             <p className="footer__license">Copyright © 2018 Yanwen Wang</p>
+            <div>
+                <a href="https://www.linkedin.com/in/yanwenwang" target="_blank" className="footer__icon">
+                    <i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/Yanwenwang" target="_blank" className="footer__icon">
+                    <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                </a>
+                <a href="https://www.instagram.com/ywphotos" target="_blank" className="footer__icon footer__icon--last">
+                    <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                </a>
+            </div>
         </footer>
     )
 };
