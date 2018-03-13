7e3e74438a4038b49b16f48158f82fbc64000e76
Yanwen <ywwenwang@gmail.com>
2018-03-13 11:30:17 -0700

    Add noreferrer keyword to hyperlinks.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
         <footer className="footer__container">
             <p className="footer__license">Copyright © 2018 Yanwen Wang</p>
             <div>
-                <a href="https://www.linkedin.com/in/yanwenwang" target="_blank" className="footer__icon">
+                <a href="https://www.linkedin.com/in/yanwenwang" rel="noopener noreferrer" target="_blank" className="footer__icon">
                     <i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
                 </a>
-                <a href="https://github.com/Yanwenwang" target="_blank" className="footer__icon">
+                <a href="https://github.com/Yanwenwang" rel="noopener noreferrer" target="_blank" className="footer__icon">
                     <i className="fa fa-github fa-3x" aria-hidden="true"></i>
                 </a>
-                <a href="https://www.instagram.com/ywphotos" target="_blank" className="footer__icon footer__icon--last">
+                <a href="https://www.instagram.com/ywphotos" rel="noopener noreferrer" target="_blank" className="footer__icon footer__icon--last">
                     <i className="fa fa-instagram fa-3x" aria-hidden="true"></i>
                 </a>
             </div>
