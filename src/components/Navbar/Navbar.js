93e4f2ed348bcecd696cae085fb41c8cf08ba2a2
Yanwen <ywwenwang@gmail.com>
2018-03-13 11:42:13 -0700

    Remove home from navbar.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,19 +8,16 @@ export const Navbar = () => {
     return (
         <div className="navbar__outer-container">
             <Container>
-            	<nav className="navbar__container">
+                <nav className="navbar__container">
                     <Link className="navbar__logo" to="/">.YW</Link>
                     <ul className="navbar__group">
                         <li>
-                        	<Link className="navbar__item" to="/">Home</Link>
-                        </li>
-                        <li>
-                        	<Link className="navbar__item" to="/resume">Resume</Link>
+                            <Link className="navbar__item" to="/resume">Resume</Link>
                         </li>
                     </ul>
                  </nav>
              </Container>
-         </div>
+        </div>
     )
 };
 
